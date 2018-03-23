38abcb678415e3828aaaeaec0df219604ea62e2e
David Peralta <david.miranda@hulu.com>
2018-03-23 10:21:42 -0600

    init react app

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 let express = require('express');
-
+let path = require("path");
 let app = express();
 let port = 8080;
 
-app.get("/", (req, res) => res.json({message: "Welcome to our Bookstore!"}));
+// export PUBLIC_URL="/" on env
+app.use("/static", express.static(__dirname + '/build/static'));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname + "/build/index.html")));
+
 app.get("/images/", (req, res) => res.json([]));
 app.get("/images/:id/", (req, res) => res.json({id: req.params.id}));
 app.post("/images/", (req, res) => res.json({id: 0}));
