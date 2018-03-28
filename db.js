5e67f529515ead4ec5cf26beaa2c0b85c75795d8
amnesiac <amnesiac.tv@gmail.com>
2018-03-28 15:27:26 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 let mysql = require('mysql');
 
 exports.connection = mysql.createConnection({
-  host: 'album.cc3m6plp6moi.us-east-2.rds.amazonaws.com',
-  user: 'ddperalta',
-  password: 'ddperalta1234',
-  database: 'album'
+  host: process.env.HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
